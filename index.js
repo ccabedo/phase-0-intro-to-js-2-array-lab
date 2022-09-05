33732cebecb9d4d2e58b9ab0dd7c77196dcc4fe8
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-05 10:40:21 -0400

    commit tree 55d66b9f92edc8769952506e340c9c93399f9584

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+console.log(cats);
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+    console.log(destructivelyAppendCat);
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+    console.log(destructivelyPrependCat);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    console.log(destructivelyRemoveFirstCat);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    console.log(destructivelyRemoveLastCat);
+}
+
+function appendCat () {
+    const appendCat = [...cats, "Broom"];
+    console.log(appendCat)
+}
+
+function prependCat () {
+   const prependCat = ["Arnold", ...cats];
+   console.log(prependCat)
+}
+
+function removeLastCat(){
+    cats.slice(2);
+    console.log(removeLastCat);
+}
+
+function removeFirstCat(){
+    cats.slice(0);
+    console.log(removeFirstCat);
+}
